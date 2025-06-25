071ef794dd754fb18284cc2d7e04dd65b0ebc977
Aswin Manoj <107464462+zephonitis@users.noreply.github.com>
2025-06-25 14:36:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, jsonify
 import joblib
 import numpy as np
+from flask_cors import CORS
 
 model = joblib.load("model.pkl")
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/predict', methods=['POST'])
 def predict():
